372d22fb16ec66b1bb29d27d068a6914eb09c61e
Billy Scrimsher <marth1987@gmail.com>
2018-05-21 20:30:38 -0500

    Hard Coded GPS Value
    The GPS Location is hard coded, but the app works, so far.
    It Needs Styling, though.

--- app/src/main/java/com/example/openweatherapp/MainActivity.java
+++ app/src/main/java/com/example/openweatherapp/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.openweatherapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView cityField, detailsField, currentTemperatureField, humidity_field, pressure_field, updatedField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        cityField = (TextView)findViewById(R.id.city_field);
+        updatedField = (TextView)findViewById(R.id.updated_field);
+        detailsField = (TextView)findViewById(R.id.details_field);
+        currentTemperatureField = (TextView)findViewById(R.id.current_temperature_field);
+        humidity_field = (TextView)findViewById(R.id.humidity_field);
+        pressure_field = (TextView)findViewById(R.id.pressure_field);
+
+
+        Weather.placeIdTask asyncTask =new Weather.placeIdTask(new Weather.AsyncResponse() {
+            public void processFinish(String weather_city, String weather_description, String weather_temperature, String weather_humidity, String weather_pressure, String weather_updatedOn) {
+
+                cityField.setText(weather_city);
+                updatedField.setText(weather_updatedOn);
+                detailsField.setText(weather_description);
+                currentTemperatureField.setText(weather_temperature);
+                humidity_field.setText("Humidity: "+weather_humidity);
+                pressure_field.setText("Pressure: "+weather_pressure);
+            }
+        });
+
+//        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+//        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//        double longitude = location.getLongitude();
+//        double latitude = location.getLatitude();
+//        System.out.println("Latitude is: " + latitude);
+//        System.out.println("Longitude is: " + longitude);
+
+        asyncTask.execute("44.8113", "-91.498497"); //  asyncTask.execute("Latitude", "Longitude")
+        //asyncTask.execute(String.valueOf(latitude),String.valueOf(longitude)); //  asyncTask.execute("Latitude", "Longitude")
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/openweatherapp/Weather.java
@@ -0,0 +1,121 @@
+package com.example.openweatherapp;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+    public class Weather {
+
+        private static final String OPEN_WEATHER_MAP_URL =
+                "http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&units=metric";
+
+        private static final String OPEN_WEATHER_MAP_API = "9c77026c7e309404f3f70034e652ab04";
+        // Insert your Weather API Here
+
+        public interface AsyncResponse {
+
+            void processFinish(String output1, String output2, String output3, String output4, String output5, String output6);
+        }
+
+
+        public static class placeIdTask extends AsyncTask<String, Void, JSONObject> {
+
+            public AsyncResponse delegate = null;
+            //Call back interface
+
+            public placeIdTask(AsyncResponse asyncResponse) {
+                delegate = asyncResponse;
+                //Assigning call back interface through constructor
+            }
+
+            @Override
+            protected JSONObject doInBackground(String... params) {
+
+                JSONObject jsonWeather = null;
+                try {
+                    jsonWeather = getWeatherJSON(params[0], params[1]);
+                } catch (Exception e) {
+                    Log.d("Error", "Cannot process JSON results", e);
+                }
+
+
+                return jsonWeather;
+            }
+
+            @Override
+            protected void onPostExecute(JSONObject json) {
+                try {
+                    if(json != null){
+                        JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+                        JSONObject main = json.getJSONObject("main");
+                        DateFormat df = DateFormat.getDateTimeInstance();
+
+
+                        String city = json.getString("name").toUpperCase(Locale.US) + ", " + json.getJSONObject("sys").getString("country");
+                        String description = details.getString("description").toUpperCase(Locale.US);
+
+                        //String Magic to Convert Temperature
+                        Double originalTemp = main.getDouble("temp");
+                        originalTemp = (originalTemp.doubleValue()*9/5)+32;
+                        String temperature = String.format("%.2f", originalTemp)+ "°";
+
+                        String humidity = main.getString("humidity") + "%";
+                        String pressure = main.getString("pressure") + " hPa";
+                        String updatedOn = df.format(new Date(json.getLong("dt")*1000));
+
+                        delegate.processFinish(city, description, temperature, humidity, pressure, updatedOn);
+
+                    }
+                } catch (JSONException e) {
+
+                }
+
+
+
+            }
+        }
+
+        public static JSONObject getWeatherJSON(String lat, String lon){
+            try {
+                URL url = new URL(String.format(OPEN_WEATHER_MAP_URL, lat, lon));
+                HttpURLConnection connection =
+                        (HttpURLConnection)url.openConnection();
+
+                connection.addRequestProperty("x-api-key", OPEN_WEATHER_MAP_API);
+
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
+
+                StringBuffer json = new StringBuffer(1024);
+                String tmp="";
+                while((tmp=reader.readLine())!=null)
+                    json.append(tmp).append("\n");
+                reader.close();
+
+                JSONObject data = new JSONObject(json.toString());
+
+                // This value will be 404 if the request was not successful
+                if(data.getInt("cod") != 200){
+                    return null;
+                }
+
+                return data;
+            }catch(Exception e){
+                return null;
+            }
+        }
+
+
+
+
+    }
+
