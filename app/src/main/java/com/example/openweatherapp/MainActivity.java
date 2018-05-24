dea4e5f888663e9a06d59e64b827344aab4c3836
Billy Scrimsher <marth1987@gmail.com>
2018-05-24 00:55:46 -0500

    Refactoring
    Get the Location before everything else, and then go crazy with making it work. Look for the ReadMe Function and invesitgate how to make that work properly.

--- app/src/main/java/com/example/openweatherapp/MainActivity.java
+++ app/src/main/java/com/example/openweatherapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView cityField, detailsField, currentTemperatureField, humidity_field, pressure_field, updatedField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,28 +16,44 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-        cityField = (TextView)findViewById(R.id.city_field);
-        updatedField = (TextView)findViewById(R.id.updated_field);
-        detailsField = (TextView)findViewById(R.id.details_field);
-        currentTemperatureField = (TextView)findViewById(R.id.current_temperature_field);
-        humidity_field = (TextView)findViewById(R.id.humidity_field);
-        pressure_field = (TextView)findViewById(R.id.pressure_field);
+        cityField = (TextView) findViewById(R.id.city_field);
+        updatedField = (TextView) findViewById(R.id.updated_field);
+        detailsField = (TextView) findViewById(R.id.details_field);
+        currentTemperatureField = (TextView) findViewById(R.id.current_temperature_field);
+        humidity_field = (TextView) findViewById(R.id.humidity_field);
+        pressure_field = (TextView) findViewById(R.id.pressure_field);
 
 
-        Weather.placeIdTask asyncTask =new Weather.placeIdTask(new Weather.AsyncResponse() {
+        Weather.placeIdTask asyncTask = new Weather.placeIdTask(new Weather.AsyncResponse() {
             public void processFinish(String weather_city, String weather_description, String weather_temperature, String weather_humidity, String weather_pressure, String weather_updatedOn) {
-
+//
                 cityField.setText(weather_city);
                 updatedField.setText(weather_updatedOn);
                 detailsField.setText(weather_description);
                 currentTemperatureField.setText(weather_temperature);
-                humidity_field.setText("Humidity: "+weather_humidity);
-                pressure_field.setText("Pressure: "+weather_pressure);
+//                humidity_field.setText("Humidity: "+weather_humidity);
+//                pressure_field.setText("Pressure: "+weather_pressure);
             }
         });
 
-//        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+//        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//        System.out.println("I'm the lm :" + lm);
+
+
+        //TODO: Find out why this function hates everyone.
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
 //        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//        System.out.println("I'm the location :" + location);
+
 //        double longitude = location.getLongitude();
 //        double latitude = location.getLatitude();
 //        System.out.println("Latitude is: " + latitude);
